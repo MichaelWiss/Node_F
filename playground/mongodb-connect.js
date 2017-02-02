e6ed04774782258258462ee4a5935534ac5cc7d3
MichaelWiss <michael.wiss@gmail.com>
2017-02-02 12:41:29 -0600

    destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
 	if (err) {
@@ -6,15 +8,25 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
    }
    console.log('Connected to MongoDB server');
 
-   db.collection('Todos').insertOne({
-   	 text: "Something to do",
-   	 completed: false
+   // db.collection('Todos').insertOne({
+   // 	 text: "Something to do",
+   // 	 completed: false
+   // }, (err, result) => {
+   // 	if (err) {
+   //     return console.log('Unable to insert todo', err);
+   // 	}
+   // 	console.log(JSON.stringify(result.ops, undefined, 2));
+   // });
+
+   db.collection('Users').insertOne({
+   	 name: "Michael",
+   	 age: 45,
+   	 location: "Brooklyn"
    }, (err, result) => {
    	if (err) {
        return console.log('Unable to insert todo', err);
    	}
-   	console.log(JSON.stringify(result.ops, undefined, 2));
+   	console.log(result.ops[0]._id.getTimestamp());
    });
-
    db.close();
 });
