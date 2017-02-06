fa4aa1fcd5fe0beb1ff25fc20bfabac6face4939
MichaelWiss <michael.wiss@gmail.com>
2017-02-06 11:50:17 -0600

    delete query

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,24 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
+	if (err) {
+   return console.log('Unable to connect to MongoDB server');
+   }
+   console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').deleteMany({text: 'Something to do'}).then((result) =>{
+  //    console.log(result);
+  // });
+
+
+  db.collection('Users').deleteOne({name: 'Andrew'}).then((result) => {
+      console.log(result);
+   // db.close();
+});
+
+   
+   // db.close();
+});
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -19,6 +19,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
       console.log(JSON.stringify(docs, undefined, 2));
    });
 
-   
-   // db.close();
+
 });
