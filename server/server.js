9372619d7f1be88f8dc1aa1f2c06ebbd60511247
MichaelWiss <michael.wiss@gmail.com>
2017-02-06 14:43:42 -0600

    mongoose records

--- server/server.js
+++ server/server.js
@@ -16,12 +16,24 @@ var Todo = mongoose.model('Todo', {
 	}
 });
 
+// var newTodo = new Todo({
+// 	text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//    console.log('Saved todo', doc);
+// }, (e) => {
+// 	console.log('Unable to save todo')
+// });
+
 var newTodo = new Todo({
-	text: 'Cook dinner'
+	text: 'Work out',
+	completed: false,
+	completedAt: 0.00
 });
 
 newTodo.save().then((doc) => {
-   console.log('Saved todo, doc');
+   console.log('Saved todo', doc);
 }, (e) => {
 	console.log('Unable to save todo')
 });
