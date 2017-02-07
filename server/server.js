6b23e36df368c593bde5915dee57f259b028b63d
MichaelWiss <michael.wiss@gmail.com>
2017-02-07 12:39:35 -0600

    db and models

--- server/server.js
+++ server/server.js
@@ -1,8 +1,4 @@
-var mongoose = require('mongoose');
-
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('/db/mongoose');
 
 var Todo = mongoose.model('Todo', {
 	text: {
