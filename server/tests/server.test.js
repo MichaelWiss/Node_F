0282a7623ca827ced840dc7c2cc489b449008cf0
MichaelWiss <michael.wiss@gmail.com>
2017-02-07 17:59:15 -0600

    configure tests in package file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,5 +12,18 @@ describe('Post /todos', () => {
 	     .post('/todos')
 	     .send({text})
          .expect(200)
+         .expect((res) => {
+         	expect(res.body.text).toBe(text);
+         })
+         .end((err, res) => {
+            if(err) {
+            	return done(err);
+            }
+            Todo.find().then((todos) => {
+            	expect(todos.length).toBe(1);
+            	expect(todos[0].text).toBe(text);
+            	done();
+            }).catch((e) => done(e;))
+         });
 	});
 });
