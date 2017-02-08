f446f97bab018a890b7d562be2950e57906ad845
MichaelWiss <michael.wiss@gmail.com>
2017-02-07 20:10:37 -0600

    queries

--- playground/mongoos-queries.js
+++ playground/mongoos-queries.js
@@ -1,8 +1,10 @@
 const {mongoose} = require('./../server/db/mongoose');
-const {Todo} = require('./../models/todo');
+const {Todo} = require('./../server/models/todo');
 
 var id = '589a7679dbff071d2f4947ff';
 
 Todo.find({
 	_id: id
-})
+}).then((todos) => {
+  console.log('Todos', todos);
+});
