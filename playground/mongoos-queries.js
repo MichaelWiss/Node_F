ab6e5ae779311d252662dd8c7ca965b7e62990ce
MichaelWiss <michael.wiss@gmail.com>
2017-02-07 20:16:55 -0600

    findById

--- playground/mongoos-queries.js
+++ playground/mongoos-queries.js
@@ -8,3 +8,13 @@ Todo.find({
 }).then((todos) => {
   console.log('Todos', todos);
 });
+
+Todo.findOne({
+	_id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  console.log('Todo By Id', todo);
+});
