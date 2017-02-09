eb9ecbc8548e93ca9bdeb70db1d4d67cab762412
MichaelWiss <michael.wiss@gmail.com>
2017-02-09 15:12:05 -0600

    remove method

--- playground/mongoose.remove.js
+++ playground/mongoose.remove.js
@@ -4,3 +4,7 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
+
+Todo.remove({}).then((result) => {
+   console.log(result);
+});
