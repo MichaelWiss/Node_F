5ebf8bfe32cfa04355e580e6452e7b47a5e4d0a1
MichaelWiss <michael.wiss@gmail.com>
2017-02-10 14:33:57 -0600

    complete delete testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -117,10 +117,19 @@ describe('Delete /todos/:id', () => {
          }).catch((e) => done(e));
        });
     });
-    // it('should return a 404 if todo not found', (done)=> {
+   it('should return a 404 if todo not found', (done)=> {
+    var hexId = new ObjectID().toHexString();
+	       request(app)
+		       .delete(`/todos/${hexId}`)
+		       .expect(404)
+		       .end(done);
       
-    // });
-    // it('should return 404 if object id is invalid', (done)=> {
+   });
+   it('should return 404 if object id is invalid', (done)=> {
+    	   request(app)
+           .delete('/todos/123abc')
+           .expect(404)
+           .end(done);
 
-    // });
+    });
 });
