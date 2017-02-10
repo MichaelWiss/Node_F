1c8892d3a967c0d24e050959811e8b051f20fb55
MichaelWiss <michael.wiss@gmail.com>
2017-02-10 14:30:41 -0600

    test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,12 +99,28 @@ describe('GET /todos/:id', () => {
 
 describe('Delete /todos/:id', () => {
     it('should remove a todo', (done) => {
-      
-    });
-    it('should return a 404 if todo not found', (done)=> {
-      
-    });
-    it('should return 404 if object id is invalid', (done)=> {
+       var hexId= todos[1]._id.toHexString();
 
+       request(app)
+       .delete(`/todos/${hexId}`)
+       .expect(200)
+       .expect((res) => {
+         expect(res.body.todo._id).toBe(hexId);
+       })
+       .end((err, res) => {
+         if (err) {
+         	return done(err);
+         }
+         Todo.findById(hexId).then((todo) => {
+         	expect(todo).toNotExist();
+         	done();
+         }).catch((e) => done(e));
+       });
     });
+    // it('should return a 404 if todo not found', (done)=> {
+      
+    // });
+    // it('should return 404 if object id is invalid', (done)=> {
+
+    // });
 });
