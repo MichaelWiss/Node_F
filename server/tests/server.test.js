e308426c77888d24e6d0c768d1e612b40308dabc
MichaelWiss <michael.wiss@gmail.com>
2017-02-11 14:03:58 -0600

    tests passing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -158,6 +158,23 @@ describe('PATCH /todos/:id', () => {
 	});
 
 	it('should clear completedAt when todo is not completed', (done) => {
+			var hexId= todos[1]._id.toHexString();
+			var text = 'This should be the new text!!';
+
+			request(app)
+				.patch(`/todos/${hexId}`)
+				.send({
+					completed: false,
+					text
+				})
+				.expect(200)
+				.expect((res) => {
+					expect(res.body.todo.text).toBe(text);
+					expect(res.body.todo.completed).toBe(false);
+		            expect(res.body.todo.completedAt).toNotExist();
+				})
+				.end(done);
+
 
 	});
 });
