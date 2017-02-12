701bec15a97206d2ef77fa8805a9f68bd2fc23f4
MichaelWiss <michael.wiss@gmail.com>
2017-02-12 17:49:12 -0600

    user save

--- server/models/user.js
+++ server/models/user.js
@@ -38,6 +38,10 @@ UserSchema.methods.generateAuthToken = function () {
    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
    user.tokens.push({access, token});
+   
+   return user.save().then(() => {
+   	return token;
+   });
 };
 
 
