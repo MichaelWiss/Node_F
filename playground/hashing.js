77d325248efc648b2ed3e4f9dd0ff6c42ba0d6f2
MichaelWiss <michael.wiss@gmail.com>
2017-02-12 16:02:17 -0600

    decoded payload

--- playground/hashing.js
+++ playground/hashing.js
@@ -8,6 +8,9 @@ var data = {
 var token = jwt.sign(data, '123abc');
 console.log(token);
 
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+
 
 // var message = "I am user 7";
 
