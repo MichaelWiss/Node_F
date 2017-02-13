aef3317720887eb9c0f9e72ffb5bb46f8eaa4a9e
MichaelWiss <michael.wiss@gmail.com>
2017-02-12 20:08:06 -0600

    hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -5,7 +5,9 @@ const bcrypt = require('bcryptjs');
 var password = '123abc!';
 
 bcrypt.genSalt(10, (err, salt) => {
-  bcrypt.hash(password, salt)
+  bcrypt.hash(password, salt, (err, hash) => {
+  	console.log(hash);
+  });
 });
 
 // var data = {
