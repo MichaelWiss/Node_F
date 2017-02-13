069edeec6e4588c9fd9f13ea86a291be3589ad08
MichaelWiss <michael.wiss@gmail.com>
2017-02-12 19:56:02 -0600

    token works

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const  {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -128,24 +129,10 @@ app.post('/users', (req, res) => {
 
 });
 
-var authenticate = (req, res, next) => {
-  var token = req.header('x-auth');
 
-  User.findByToken(token).then((user) => {
-    if (!user) {
-      return Promise.reject();
-    }
-
-   req.user = user;
-   req.token = token;
-   next();
-  }).catch((e) => {
-    res.status(401).send();
-  });
-
-};
 
 //private route
+
 
 app.get('/users/me', authenticate, (req, res) => {
  res.send(req.user);
