6d4869bbfe519aedf571cc978555b4fdcd1a0eba
MichaelWiss <michael.wiss@gmail.com>
2017-02-17 13:31:47 -0600

    user login method

--- server/models/user.js
+++ server/models/user.js
@@ -72,14 +72,20 @@ UserSchema.statics.findByToken = function (token) {
 UserSchema.statics.findByCredentials = function (email, password) {
    var User = this;
 
-   User.findOne({email}).then((user) => {
+   return User.findOne({email}).then((user) => {
      if (!user) {
         return Promise.reject();
       }
 
       return new Promise((resolve, reject) => {
-        
-      })
+        bcrypt.compare(password, user.password, (err, res) => {
+           if(res) {
+             resolve(user);
+           } else {
+            reject();
+           }
+        });
+      });
    });
 };
 
