6c9b0d28dd931c93ca61cef7ff185a9c31172f0e
MichaelWiss <michael.wiss@gmail.com>
2017-03-04 20:38:31 -0600

    making route private

--- server/server.js
+++ server/server.js
@@ -59,15 +59,18 @@ app.get('/todos/:id', authenticate, (req, res) => {
   });
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
   return res.status(404).send();
     }
+
 Todo.findByIdAndRemove(id).then((todo) => {
      if (!todo) {
       return res.status(404).send();
   }
+
   res.send({todo});
 }).catch((e) => {
   res.status(400).send();
